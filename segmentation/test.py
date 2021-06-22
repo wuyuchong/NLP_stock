31899c5d2976d0ce0cea811894c366b5666b1497
wuyuchong(NLP) <vector_wyc@icloud.com>
2021-06-22 16:54:30 +0800

    limit filesize

--- segmentation/test.py
+++ segmentation/test.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import hanlp
 import json
-with open('/data/news/格力电器/CNY1MmUBCy8qJGFBg9hn.txt') as f:
-    data = f.read()
-js = json.loads(data)
-print(js['web'])
+import ast
+import sys
+import os
+
+#  HanLP = hanlp.load(hanlp.pretrained.mtl.CLOSE_TOK_POS_NER_SRL_DEP_SDP_CON_ELECTRA_SMALL_ZH)
+
+size = os.path.getsize('/data/news/豪悦护理/f5041bf0d22cbd0d3d7f0951aa15a18b.txt')
+print(size > 100000)
+sys.exit()
+
+file = open('/data/news/豪悦护理/f5041bf0d22cbd0d3d7f0951aa15a18b.txt')
+contents = file.read()
+#  dictionary = ast.literal_eval(contents)
+dictionary = json.loads(contents)
+title = dictionary['title']
+content = dictionary['content']
+file.close()
+
+data = HanLP(content, tasks=['tok/fine', 'tok/coarse'])
+print(data)
 
